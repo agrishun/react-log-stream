d6e0936eda4732ec0101acb9318fae8870c1b21e
Anton Grishun <agrishun@me.com>
2017-02-15 16:40:47 +0100

    Add dist files

---
+++ dist/index.js
@@ -0,0 +1,14 @@
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.default = undefined;
+
+var _LogStream = require('./LogStream');
+
+var _LogStream2 = _interopRequireDefault(_LogStream);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+exports.default = _LogStream2.default;
